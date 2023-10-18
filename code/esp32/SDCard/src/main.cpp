1889b772d1dcf6e7a4d4b043595657140fad95df
Fathi Abdelmalek <abdelmalek.fathi.2001@gmail.com>
2023-10-18 23:49:29 +0100

    now it's time to know how to use sd card

---
+++ code/esp32/SDCard/src/main.cpp
@@ -0,0 +1,26 @@
+#include "SDCardManager.h"
+
+void setup(){
+  Serial.begin(115200);
+  setupSDCard();
+  // listDir(SD, "/", 0);
+  // createDir(SD, "/mydir");
+  // listDir(SD, "/", 0);
+  // removeDir(SD, "/mydir");
+  // listDir(SD, "/", 2);
+  // writeFile(SD, "/hello.txt", "Hello ");
+  // appendFile(SD, "/hello.txt", "World!\n");
+  // readFile(SD, "/hello.txt");
+  // deleteFile(SD, "/foo.txt");
+  // renameFile(SD, "/hello.txt", "/foo.txt");
+  // readFile(SD, "/foo.txt");
+  // testFileIO(SD, "/test.txt");
+}
+
+void loop(){
+  listDir(SD, "/", 0);
+  Serial.printf("Card size: %lluMB\n", SD.cardSize() / (1024 * 1024));
+  Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
+  Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
+  delay(5000);
+}
